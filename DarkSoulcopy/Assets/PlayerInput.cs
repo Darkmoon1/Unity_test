66f4126abff17d82590a9d898fce255add2b679b
Darkmoon1 <864275144@qq.com>
2018-05-03 10:19:48 +0800

    18/5/3
    学习黑魂复刻第一天

---
+++ DarkSoulcopy/Assets/PlayerInput.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+    public string keyUP = "w";
+    public string keyDown = "s";
+    public string keyLeft = "a";
+    public string keyRight = "d";
+    // Use this for initialization
+
+    public float Dup;
+    public float Dright;
+    public float Dmag;
+    public Vector3 Dvec;
+
+    public bool inputEnabled = true;
+
+    private float targetDup;
+    private float targetDright;
+
+    private float velocityDup;
+    private float velocityDright;
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+
+
+	void Update () {
+        targetDright = (Input.GetKey(keyLeft) ? 1.0f : 0) - (Input.GetKey(keyRight) ? 1.0f : 0);
+        targetDup = (Input.GetKey(keyUP) ? 1.0f : 0) - (Input.GetKey(keyDown) ? 1.0f : 0);
+
+        if (!inputEnabled)
+        {
+            targetDup = 0;
+            targetDright = 0;
+        }
+ 
+        Dup = Mathf.SmoothDamp(Dup, targetDup, ref velocityDup, 0.1f);
+        Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, 0.1f);
+        Dmag = Mathf.Sqrt((Dup * Dup) + (Dright * Dright));
+        Dvec = Dright * transform.right + Dup * transform.forward;
+
+
+    }
+}
---
+++ DarkSoulcopy/Assets/actorController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class actorController : MonoBehaviour {
+    public GameObject model;
+    public PlayerInput pi;
+    public float moveSpeed = 1.4f;
+
+    [SerializeField]
+    private Animator anim;
+    private Rigidbody rigid;
+    private Vector3 movingVec;
+
+	// Use this for initialization
+	void Awake () {
+        anim = model.GetComponent<Animator>();
+        pi = GetComponent<PlayerInput>();
+        rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame 1/60
+	void Update () {
+        anim.SetFloat("forward", pi.Dmag);
+        if(pi.Dmag > 0.1f)
+            model.transform.forward = pi.Dvec;
+        movingVec = pi.Dmag * model.transform.forward;
+	}
+    //called per frame 1/50
+    void FixedUpdate(){
+        rigid.position += movingVec * Time.fixedDeltaTime * moveSpeed;
+    }
+}
