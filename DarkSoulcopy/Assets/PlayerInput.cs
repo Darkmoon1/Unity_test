d936ce18b7966f062e9f01d255c3486370f236ae
Darkmoon1 <864275144@qq.com>
2018-05-07 22:22:42 +0800

    18/5/7
    进一步学习

--- DarkSoulcopy/Assets/PlayerInput.cs
+++ DarkSoulcopy/Assets/PlayerInput.cs
@@ -2,16 +2,30 @@
 using System.Collections;
 
 public class PlayerInput : MonoBehaviour {
+
+    [Header("Key Settings")]
     public string keyUP = "w";
     public string keyDown = "s";
     public string keyLeft = "a";
     public string keyRight = "d";
     // Use this for initialization
+    public string keyA = "left shift";    
+    public string keyB = "space";
+    public string keyC;
+    public string keyD;
 
+    [Header("OutPut Signal")]
     public float Dup;
     public float Dright;
     public float Dmag;
     public Vector3 Dvec;
+
+    //1.pressing signal 2.trigger once signal 3.double trigger
+    public bool run = false;
+    public bool jump;
+    private bool lastJump;
+
+    [Header("Others")]
 
     public bool inputEnabled = true;
 
@@ -39,9 +53,26 @@ public class PlayerInput : MonoBehaviour {
  
         Dup = Mathf.SmoothDamp(Dup, targetDup, ref velocityDup, 0.1f);
         Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, 0.1f);
-        Dmag = Mathf.Sqrt((Dup * Dup) + (Dright * Dright));
-        Dvec = Dright * transform.right + Dup * transform.forward;
 
+        Vector2 tempDAxis = SquareToCiracle(Dright, Dup);
+        float Dup2 = tempDAxis.y;
+        float Dright2 = tempDAxis.x;
 
+        Dmag = Mathf.Sqrt((Dup2 * Dup2) + (Dright2 * Dright2));
+        Dvec = Dright2 * transform.right + Dup2 * transform.forward;
+
+        run = Input.GetKey(keyA);
+
+        jump = Input.GetKeyDown(keyB);
+    }
+
+    private Vector2 SquareToCiracle(float Dright,float Dup)
+    {
+        Vector2 outPut = Vector2.zero;
+
+        outPut.x = Dright * Mathf.Sqrt(1 - (Dup * Dup) / 2.0f);
+        outPut.y = Dup * Mathf.Sqrt(1 - (Dright * Dright) / 2.0f);
+
+        return outPut;
     }
 }
--- DarkSoulcopy/Assets/actorController.cs
+++ DarkSoulcopy/Assets/actorController.cs
@@ -4,12 +4,18 @@ using System.Collections;
 public class actorController : MonoBehaviour {
     public GameObject model;
     public PlayerInput pi;
-    public float moveSpeed = 1.4f;
+    public float moveSpeed = 2.0f;
+    public float runSpeed = 2.5f;
+    public float rollSpeed = 1.0f;
 
     [SerializeField]
     private Animator anim;
     private Rigidbody rigid;
     private Vector3 movingVec;
+    private Vector3 thrustVector;
+
+    private bool lockMoving = false;
+
 
 	// Use this for initialization
 	void Awake () {
@@ -20,13 +26,65 @@ public class actorController : MonoBehaviour {
 	
 	// Update is called once per frame 1/60
 	void Update () {
-        anim.SetFloat("forward", pi.Dmag);
+        anim.SetFloat("forward", pi.Dmag * Mathf.Lerp(anim.GetFloat("forward"), ((pi.run) ? runSpeed : 1.0f), 0.4f));
         if(pi.Dmag > 0.1f)
-            model.transform.forward = pi.Dvec;
-        movingVec = pi.Dmag * model.transform.forward;
+        {
+            model.transform.forward = Vector3.Slerp(model.transform.forward, pi.Dvec, 0.3f); 
+        }
+        if (pi.jump)
+        {
+            anim.SetTrigger("jump");
+        }
+        if (lockMoving == false)
+        {
+            movingVec = pi.Dmag * model.transform.forward * moveSpeed * Mathf.Lerp(anim.GetFloat("forward"), ((pi.run) ? runSpeed : 1.0f), 0.4f);
+        }
+        if(rigid.velocity.magnitude > 5.0f)
+        {
+            anim.SetTrigger("roll");
+        }
+
 	}
     //called per frame 1/50
     void FixedUpdate(){
-        rigid.position += movingVec * Time.fixedDeltaTime * moveSpeed;
+        rigid.velocity = new Vector3(movingVec.x, rigid.velocity.y, movingVec.z) + thrustVector;
+        thrustVector = Vector3.zero;
     }
+    /*
+     * message processing..
+     *
+     **/ 
+     
+
+    public void onJumpEnter()
+    {
+        pi.inputEnabled = false;
+        lockMoving = true;
+        thrustVector = new Vector3(0, 3.5f, 0);
+    }
+
+    public void isGround()
+    {
+        anim.SetBool("isGround", true);
+    }
+    public void isNotGround()
+    {
+        pi.inputEnabled = false;
+        lockMoving = true;
+        anim.SetBool("isGround", false);
+    }
+
+    public void onGroundEnter()
+    {
+        pi.inputEnabled = true;
+        lockMoving = false;
+    }
+
+    public void onRollEnter()
+    {
+        pi.inputEnabled = true;
+        lockMoving = false;
+        thrustVector = new Vector3(0, rollSpeed, 0 );
+    }
+
 }
